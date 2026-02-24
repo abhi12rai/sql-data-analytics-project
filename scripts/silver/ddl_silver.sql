4ac8732c002f16c1fc8272659a5e8a791adc0b36
Abhishek Rai <abhisekhrai3@gmail.com>
2026-02-24 15:57:48 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,13 +1,4 @@
-/*
-===============================================================================
-DDL Script: Create Silver Tables
-===============================================================================
-Script Purpose:
-    This script creates tables in the 'silver' schema, dropping existing tables 
-    if they already exist.
-	  Run this script to re-define the DDL structure of 'bronze' Tables
-===============================================================================
-*/
+
 -- DDL Script: Create silver Tables
 
 IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL  
@@ -95,6 +86,44 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     subcat              NVARCHAR(50),
     maintenance         NVARCHAR(50),
     dwh_creat_date      DATETIME2 DEFAULT GETDATE() 
+);
+GO
+
+
+-- During Data Cleaning & Loading, there is some changes in Meta Data of table silver.crm_prd_info & silver.crm_sales_details 
+
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_prd_info;
+GO
+
+CREATE TABLE silver.crm_prd_info (
+    prd_id          INT,
+    cat_id          NVARCHAR(50),
+    prd_key         NVARCHAR(50),
+    prd_nm          NVARCHAR(50),
+    prd_cost        INT,
+    prd_line        NVARCHAR(50),
+    prd_start_dt    DATE,
+    prd_end_dt      DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE silver.crm_sales_details;
+GO
+
+CREATE TABLE silver.crm_sales_details (
+    sls_ord_num     NVARCHAR(50),
+    sls_prd_key     NVARCHAR(50),
+    sls_cust_id     INT,
+    sls_order_dt    DATE,
+    sls_ship_dt     DATE,
+    sls_due_dt      DATE,
+    sls_sales       INT,
+    sls_quantity    INT,
+    sls_price       INT,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
