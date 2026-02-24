52c0636cb378652dbb9d2a532c7611ef1868b4fc
Abhishek Rai <abhisekhrai3@gmail.com>
2026-02-25 01:37:52 +0530

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -87,7 +87,7 @@ SELECT
     sd.sls_quantity AS quantity,
     sd.sls_price AS price
 FROM silver.crm_sales_details sd
-LEFT JOIN gold.dim_product pr
+LEFT JOIN gold.dim_products pr
 ON sd.sls_prd_key = pr.product_number
 LEFT JOIN gold.dim_customers cu
 ON sd.sls_cust_id = cu.customer_id;
